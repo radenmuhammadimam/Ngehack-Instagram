e367a8e50e3f683ff46a81fde4050605a6f6c961
Mr. Erningpraja <raim1104@gmail.com>
2018-11-07 17:30:20 +0700

    AppInfo.py

---
+++ Instagram/AppInfo.py
@@ -0,0 +1,11 @@
+
+
+appInfo = {
+    "version": "2.0.1",
+    "name": "Instagram-Py",
+    "description": " Slick Instagram brute force command line tool",
+    "author": "DeathSec",
+    "company": "The Future Shell",
+    "year": "2017",
+    "example": "example: instagram-py -c -vvv -u instatestgod__ -pl rockyou.txt"
+}
